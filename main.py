edac8ee8e41f6b2d1411a4d9f79079aacc6ebba9
Guntera-ui <glekiashvili536@gmail.com>
2025-11-30 03:59:55 +0400

    Added Player ship

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH // 2
+    y = SCREEN_HEIGHT // 2
+    player = Player(x, y)
+
     while True:
-        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
-    
 
+        log_state()
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
