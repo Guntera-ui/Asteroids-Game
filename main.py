0161047cd8e2aef2025d3ed5a756129b2de7d5db
Guntera-ui <glekiashvili536@gmail.com>
2025-12-05 19:40:23 +0400

    Added Asteroid Field Generator

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
@@ -12,13 +14,15 @@ def main():
     dt = 0
     x = SCREEN_WIDTH // 2
     y = SCREEN_HEIGHT // 2
-    
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
-
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
     player = Player(x, y)
-
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -26,9 +30,9 @@ def main():
 
         log_state()
         updatable.update(dt)
-        
+
         screen.fill("black")
-        
+
         for obj in drawable:
             obj.draw(screen)
 
